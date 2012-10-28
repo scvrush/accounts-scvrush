d0ab1afb012597d4d1216251fae505e6f537bd74
Jakub Arnold <darthdeus@gmail.com>
2012-10-28 12:02:50 +0100

    Remote http authentication skeleton

--- scvrush_server.js
+++ scvrush_server.js
@@ -1,6 +1,17 @@
+if (typeof Scvrush === "undefined") Scvrush = {};
+
+Scvrush.authenticate = function(login, password, cb) {
+  return true;
+};
+
 Meteor.methods({
   authenticate: function(login, password) {
-    var token = Meteor.uuid();
-    return token;
+    var result = Scvrush.authenticate(login, password);
+    if (result) {
+      var token = Meteor.uuid();
+      return token;
+    } else {
+      return false;
+    }
   }
 });
